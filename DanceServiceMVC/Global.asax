7cdb03803bd61800448179f428f857ac375fa8c9
Misha <aweerr78@gmail.com>
2016-11-11 10:28:58 +0200

    Create MVC WebApi WPF & model

---
+++ DanceServiceMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DanceServiceMVC.MvcApplication" Language="C#" %>
---
+++ DanceServiceModel/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanceServiceModel
+{
+    public class Class1
+    {
+    }
+}
